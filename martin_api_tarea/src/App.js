8505675e395ea2e33f0d63e36ec4496a076fa611
MartinDeLaCruz.69 <martindelacruz169.com>
2022-06-27 01:20:32 -0500

    Ci: The last part of the pokemon function is added with its name and image

--- martin_api_tarea/src/App.js
+++ martin_api_tarea/src/App.js
@@ -10,12 +10,18 @@ function traerPokemon() {
   });
 }
 
-traerPokemon();
-
 function crearPokemon(pokemon){
     const img = document.createElement("img");
     img.src = pokemon.sprites.front_default;
 
     const h3 = document.createElement("h3");
     h3.textContent = pokemon.name;
+
+    const div = document.createElement("div");
+    div.appendChild(img);
+    div.appendChild(h3);
+
+    pokemonContainer.appendChild(div);
 }
+
+traerPokemon();
