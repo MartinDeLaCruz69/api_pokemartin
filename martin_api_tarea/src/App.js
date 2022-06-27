4fa6bfc74af9178499c4a98a377875d0fc41575d
MartinDeLaCruz.69 <martindelacruz169.com>
2022-06-27 01:02:42 -0500

    Feat: Added fetch function for Pokemon data

--- martin_api_tarea/src/App.js
+++ martin_api_tarea/src/App.js
@@ -1,3 +1,9 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const pokemonContainer = document.querySelector(".pokemon-container");
+
+function traerPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+}
